1f7961f73501b5ed2b86a2bc72191d9c0d6aae05
Leo Paul Larkin <lplinkl00@gmail.com>
2025-12-05 11:37:41 +0800

    trying to optimize

--- functions/main.py
+++ functions/main.py
@@ -6,6 +6,7 @@ from firebase_functions import https_fn, options
 from firebase_functions.options import set_global_options
 from firebase_admin import initialize_app
 import datetime
+import asyncio
 # For cost control, you can set the maximum number of containers that can be
 # running at the same time. This helps mitigate the impact of unexpected
 # traffic spikes by instead downgrading performance. This limit is a per-function
@@ -18,11 +19,14 @@ import pandas as pd
 from google.transit import gtfs_realtime_pb2
 from google.protobuf.json_format import MessageToDict
 import requests
-
+bus_icon = folium.Icon(color="blue", icon="bus", prefix="fa")
+usr_icon = folium.Icon(color="red", icon="person", prefix="fa")
+m = folium.Map(location=[3.1319, 101.6841], zoom_level = 10, tiles="OpenStreetMap")
 
 @https_fn.on_request(cors=options.CorsOptions(
         cors_origins="*",
         cors_methods=["get", "post", "options"],))
+        
 def load_map_on_request(req: https_fn.Request) -> https_fn.Response:
     rapid_buses_url = "https://api.data.gov.my/gtfs-realtime/vehicle-position/prasarana?category=rapid-bus-kl"
     mrt_buses_url = "https://api.data.gov.my/gtfs-realtime/vehicle-position/prasarana?category=rapid-bus-mrtfeeder"
@@ -41,16 +45,14 @@ def load_map_on_request(req: https_fn.Request) -> https_fn.Response:
         feed.ParseFromString(response.content)
         vehicle_positions = [MessageToDict(entity.vehicle) for entity in feed.entity]
         print(f'Total vehicles: {len(vehicle_positions)}')
-        df = pd.json_normalize(vehicle_positions)
-        return df
+        return pd.json_normalize(vehicle_positions)
 
     rapid_bus = get_gtfs(rapid_buses_url)
     mrt_bus = get_gtfs(mrt_buses_url)
-    m = folium.Map(location=[3.1319, 101.6841], zoom_level = 10, tiles="OpenStreetMap")
+    
     usr_coords = get_usr_location(ip_url)
     bus_rpd_fg = folium.FeatureGroup(name="Rapid KL buses")
     bus_mrt_fg = folium.FeatureGroup(name="MRT Buses")
-    bus_icon = folium.Icon(color="blue", icon="bus", prefix="fa")
 
     def get_bus_pos(df, fg):
         for s in range(len(df)):
@@ -58,17 +60,13 @@ def load_map_on_request(req: https_fn.Request) -> https_fn.Response:
             lon = df["position.longitude"].iloc[s]
             bus_id = df["vehicle.id"].iloc[s]
             folium.Marker(location=[lat, lon],icon=bus_icon, radius=4, popup=bus_id ).add_to(fg)
-        return https_fn.Response("Hello world!")
+        return
 
     get_bus_pos(rapid_bus, bus_rpd_fg)
     get_bus_pos(mrt_bus, bus_mrt_fg)
     bus_mrt_fg.add_to(m)
     bus_rpd_fg.add_to(m)
-    test_usr_location = [3.155687,101.692563]
     usr_fg = folium.FeatureGroup(name="User")
-    usr_icon = folium.Icon(color="red", icon="person", prefix="fa")
-    # folium.Marker(location=test_usr_location, icon=usr_icon, radius=4).add_to(usr_fg)
-    print(usr_coords.split(','))
+    folium.Marker(location=usr_coords.split(','), icon=usr_icon, radius=4).add_to(usr_fg)
     usr_fg.add_to(m)
-    html = m._repr_html_()
-    return https_fn.Response(html)
+    return https_fn.Response(m.get_root().render())
